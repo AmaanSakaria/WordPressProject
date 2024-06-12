16ff30b6b21f73d6702a256caccc22e4815b4f37
AmaanSakaria <126667634+AmaanSakaria@users.noreply.github.com>
2024-06-12 15:37:12 +0100

    Create Lemp-setup.sh

---
+++ Lemp-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y upgrade
+sudo touch testing.txt #this file will contain the output of our lemp stack unit tests
+sudo apt -y install nginx
+sudo systemctl strat nginx && sudo systemctl enable nginx       #this starts and enables nginx on a server reboot. the 2nd comand will only run if the first command is succesful 
+sudo status nginx > texting.txt
+sudo apt -y install mariadb server 
+sudo systemctl start mariadb && sudo systemctl enable mariadb
+sudo status mariadb >> texting.txt
+sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
+sudo php -v >> texting.txt
