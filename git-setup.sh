1d0d1633387ccefb6da34940df8b4406fc069976
AmaanSakaria <126667634+AmaanSakaria@users.noreply.github.com>
2024-07-03 12:09:42 +0100

    Update git-setup.sh
    Added update and upgrade

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt -y install git
+sudo apt -y update
+sudo apt -y upgrade
 sudo cd /root/
 sudo git clone https://github.com/AmaanSakaria/WordPressProject.git
 sudo chmod -R 755 wordpress-project
